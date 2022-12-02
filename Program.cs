3a81310299337ac3477d6f65ddb776ca6d2ef4e9
Slava Kirin <savadz1966@gmail.com>
2022-12-02 16:16:53 +0300

    seminar primer 1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// seminar primer 1
+Console.WriteLine("Введите число: ");
+int number=int.Parse(Console.ReadLine());
+//matem put
+int sqr=number*number;
+Console.WriteLine($"Квадрат числа {number} равен = {sqr}");
+
+//biblioteka
+int sqrl=Convert.ToInt32(Math.Pow(number,2));
+Console.WriteLine($"Квадрат числа {number} равен = {sqrl}");
+double number2 = 3.3;
+double sqr2=(Math.Pow(number2, 2));
+Console.WriteLine($"Квадрат числа {number} равен = {Math.Round(sqr2, 2)}");
